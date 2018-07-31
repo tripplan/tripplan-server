23b26ee0cf20f3acb5f3db10233b3fab008f8e54
linorabolini <linorabolini@gmail.com>
2018-07-31 10:56:07 +0200

    added uuid instead of number id

--- generate.js
+++ generate.js
@@ -3,37 +3,39 @@ const _ = require("lodash")
 
 const samples = 100
 
+const people = _.times(samples, () => {
+    return {
+        id: faker.random.uuid(),
+        name: faker.name.findName()
+    }
+})
+
 module.exports = () => {
     return {
-        people: _.times(samples, id => {
-            return {
-                id,
-                name: faker.name.findName()
-            }
-        }),
-        trips: _.times(samples, id => {
+        people,
+        trips: _.times(samples, () => {
             const startDate = faker.date.future(1)
             const endDate = faker.date.future(1, startDate)
             return {
-                id,
+                id: faker.random.uuid(),
                 startDate,
                 endDate,
                 destinations: [
                     {
-                        id: 0,
+                        id: faker.random.uuid(),
                         title: faker.lorem.sentence(5),
                         startDate
                     },
-                    ..._.times(faker.random.number({ min: 1, max: 5 }), id => {
+                    ..._.times(faker.random.number({ min: 1, max: 5 }), () => {
                         return {
-                            id: id + 1,
+                            id: faker.random.uuid(),
                             title: faker.address.city(),
                             startDate: faker.date.between(startDate, endDate)
                         }
                     })
                 ],
-                people: _.times(faker.random.number({ min: 1, max: 5 }), n => {
-                    return faker.random.number(samples)
+                people: _.times(faker.random.number({ min: 1, max: 5 }), () => {
+                    return faker.random.arrayElement(people).id
                 })
             }
         })
